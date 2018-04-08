a4f51a84a151b1d574f674a8b9e60a1aa96cee42
migue <miguel.casado@gmail.com>
2018-04-08 21:21:16 +0200

    Start the Peer to Peer Chat msg exchange

--- app/controllers/WebsocketController.java
+++ app/controllers/WebsocketController.java
@@ -3,8 +3,12 @@ package controllers;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import actors.MyChatActorWebRTC;
+import actors.MyWebSocketActor1;
 import akka.actor.ActorSystem;
 import akka.stream.Materializer;
+import play.Logger;
+import play.libs.streams.ActorFlow;
 import play.mvc.*;
 
 public class WebsocketController extends Controller {
@@ -21,7 +25,25 @@ public class WebsocketController extends Controller {
 	    }
 
 
-
+    public WebSocket wsMyChatPeerToPeer() {
+    	
+    	toto=toto+1;
+    	//test
+    	Logger.debug("wsMyChatPeerToPeer WS. + toto:"+toto);
+    	Logger.debug("wsMyChatPeerToPeer WS. + actorSystem="+actorSystem.name());
+    	
+        return WebSocket.Text.accept(request ->
+                ActorFlow.actorRef(MyChatActorWebRTC::props, actorSystem, materializer)
+                                  );
+    }
+	
+	
+	
+	
+	public Result webSocketChatPeerToPeer() {
+        return ok(views.html.webChatPeerToPeer.render());
+    }
+    
 
 
 
